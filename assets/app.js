bedb07c9ec94d3992c8013b2a61a1c2bcf331397
kram254 <markorlando45@gmail.com>
2021-01-27 14:21:56 +0300

    Webpack Encore

---
+++ assets/app.js
@@ -0,0 +1,29 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+import $ from 'jquery';
+
+// start the Stimulus application
+import './bootstrap';
+/**
+ * code to handle comment votes up/down
+ * */
+var $container = $('.js-votes-arrows');
+$container.find('a').on('click', function (e){
+    e.preventDefault();
+    var $link = $(e.currentTarget);
+
+    $.ajax({
+        url: '/comments/10/vote'+$link.data('direction'),
+        method: 'POST'
+    }).then(function (data){
+        $container.find('.js-vote-total').text(data.votes);
+    })
+})
